606f72c9a69e89c561d412438ce5b8111adde85f
Devamjit1776 <bhatidevamjit1776@gmail.com>
2024-09-29 21:37:50 +0530

    Add files via upload

---
+++ queries DBMS.sql
@@ -0,0 +1,34 @@
+CREATE TABLE item(
+item_id int(225),
+item_name varchar(225),
+item_price int (225),
+item_qntity int (225),
+consumer_name varchar(225));
+
+insert into item(item_id,item_name,item_price,item_qntity,consumer_name) value (1,'Shoes',6000,1,'Devam');
+insert into item(item_id,item_name,item_price,item_qntity,consumer_name) value (2,'Shampoo',600,2,'Dishaan');
+insert into item(item_id,item_name,item_price,item_qntity,consumer_name) value (3,'Diapers',1500,3,'Krishna');
+insert into item(item_id, item_name,item_price,item_qntity,consumer_name) value (4,'Kurta',1000,1,'Rashid');
+insert into item(item_id,item_name,item_price,item_qntity,consumer_name) value (5,'Maggi',60,1,'Rishab');
+insert into item(item_id,item_name,item_price,item_qntity,consumer_name) value (6,'Buds',2000,1,'vansh');
+
+
+select * from item;
+delete from item where item_id=2;
+
+select * from item;
+select min(item_price) from item;
+
+select * from item;
+select max(item_price) from item;
+
+select avg(item_price) from item;
+
+select count(item_id) from item;
+
+alter TABLE item add product_id int(225);
+
+select * from item;
+alter TABLE item drop column item_id;
+
+select * from item;
